57f5af0ad9643ab7add43e63b83179737da289f9
Ubuntu <ubuntu@vps-1f92e0e9.vps.ovh.net>
2022-07-31 14:45:36 +0000

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,57 @@
+-- ok i know ths code is hot stinky garbage but it *works*, god damn-it.
+
+-- it works most of the time. most of the time.
+_G["discordia"] = require('discordia')
+require("discordia-components")
+_G["client"] = discordia.Client()
+_G["prefix"] = "c!"
+_G["json"] = require('libs/json')
+_G["fs"] = require('fs')
+--from https://github.com/DeltaF1/lua-tracery, TODO properly follow the license lmao
+_G["tracery"] = require('libs/tracery')
+_G["dpf"] = require('libs/dpf')
+_G["inspect"] = require('libs/inspect')
+_G["vips"] = require('vips')
+_G["http"] = require('coro-http')
+
+-- load all the extensions
+discordia.extensions()
+
+-- import all the commands
+_G['cmd'] = {}
+-- import reaction commands
+_G['cmdre'] = {}
+
+_G['cmdcons'] = {}
+
+_G['tr'] = {}
+local rdb = dofile('commands/reloaddb.lua')
+rdb.run(nil,nil,true)
+print("exited rdb.run")
+
+_G['sw'] = discordia.Stopwatch()
+sw:start()
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+print("yay got past load ready")
+
+client:on('messageCreate', function(message)
+  handlemessage(message)
+end)
+
+print("Resetting clocks")
+resetclocks()
+
+print("Clearing cache")
+clearcache()
+
+print("Stocking shop")
+stockshop()
+
+print("Bot token:" .. privatestuff.botid)
+
+client:run(privatestuff.botid)
+
+client:setGame("with cards | c!help")
