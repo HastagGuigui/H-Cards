89f3559622f5a3d7b4d7f21c2398ceceac86ee13
Tiago1257 <84469804+Tiago1257@users.noreply.github.com>
2022-08-01 20:01:10 -0300

    now green

--- commands/inventory.lua
+++ commands/inventory.lua
@@ -28,7 +28,7 @@ function command.run(message, mt)
   message.channel:send{
     content = message.author.mentionString .. ", your inventory contains:",
     embed = {
-      color = 00FF00ff,
+      color = 00ff00ff,
       title = message.author.name .. "'s Inventory",
       description = invstring,
       footer = {
