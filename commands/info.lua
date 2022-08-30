961171eb18af8ce8ff46be5eafe077eb4b569b0b
Tiago1257 <84469804+Tiago1257@users.noreply.github.com>
2022-08-30 19:06:18 -0300

    c!info code

--- commands/info.lua
+++ commands/info.lua
@@ -1,1 +1,9 @@
-
+local command = {}
+function command.run(message, mt)
+  print(message.author.name .. " did !h")
+   message.channel:send{embed = {
+        color = 0x00FF00,
+        title = "About H-Cards",
+        description = "H-Cards is a closed-source fork of the RDCards bot, made exclusively for \"The H's Friends\" Discord server. \nThis bot includes custom cards, QOL features and more coming soon! Please feel free to suggest any features!\n**RDCards made by DPS2004, Random Guy JCI and BIrDIS** \n**H-Cards made by #Guigui, 1257Plays and Healion123**",
+end
+return command
